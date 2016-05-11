cdde0a4d0a0fd1d415e77ab17160c2bea1ef9067
timzaak <zsy.evan@gmail.com>
2016-05-11 22:34:42 +0800

    reslove #7

--- ios/RCTImageResizer/RCTImageResizer.m
+++ ios/RCTImageResizer/RCTImageResizer.m
@@ -44,7 +44,13 @@ RCT_EXPORT_METHOD(createResizedImage:(NSString *)path
 
     [_bridge.imageLoader loadImageWithTag:path callback:^(NSError *error, UIImage *image) {
         if (error || image == nil) {
-            image = [[UIImage alloc] initWithContentsOfFile:path];
+            UIImage* image;
+            if ([path hasPrefix:@"data:"] || [path hasPrefix:@"file:"]) {
+                NSURL *imageUrl = [[NSURL alloc] initWithString:path];
+                image = [UIImage imageWithData:[NSData dataWithContentsOfURL:imageUrl]];
+            } else {
+                image = [[UIImage alloc] initWithContentsOfFile:path];
+            }
             if (image == nil) {
                 callback(@[@"Can't retrieve the file from the path.", @""]);
                 return;
