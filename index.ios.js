79628b6ec88d820914750f784ea735a722fe521c
Andrew Shini <superandrew213@gmail.com>
2016-06-17 09:53:01 +1000

    Add outputPath for ios

--- index.ios.js
+++ index.ios.js
@@ -3,13 +3,13 @@ import {
 } from 'react-native';
 
 export default {
-  createResizedImage: (path, width, height, format, quality) => {
+  createResizedImage: (path, outputPath, width, height, format, quality) => {
     if (format !== 'JPEG') {
       throw new Error('Only JPEG format is supported by createResizedImage');
     }
 
     return new Promise((resolve, reject) => {
-      NativeModules.ImageResizer.createResizedImage(path, width, height, quality, (err, resizedPath) => {
+      NativeModules.ImageResizer.createResizedImage(path, outputPath, width, height, quality, (err, resizedPath) => {
         if (err) {
           return reject(err);
         }
