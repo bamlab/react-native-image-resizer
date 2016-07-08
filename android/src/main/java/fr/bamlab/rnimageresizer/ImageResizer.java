862c5724169e061ad699e90795a5a537cfc930a6
Daniel Dunderfelt <danieldunderfelt@gmail.com>
2016-07-08 20:37:36 +0300

    fix output path check

--- android/src/main/java/fr/bamlab/rnimageresizer/ImageResizer.java
+++ android/src/main/java/fr/bamlab/rnimageresizer/ImageResizer.java
@@ -109,7 +109,7 @@ class ImageResizer {
         Bitmap resizedImage = ImageResizer.rotateImage(ImageResizer.resizeImage(imagePath, newWidth, newHeight, context), rotation);
 
         File path = context.getCacheDir();
-        if (outputPath != null || !outputPath.isEmpty()) {
+        if (outputPath != null) {
           path = new File(outputPath);
         }
 
