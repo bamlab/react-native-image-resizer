d2c20eeef8b44afbd3672f6a0274e0e98ce92fa6
Marco Cimmino <marcim@microsoft.com>
2017-06-14 14:27:58 -0700

    Unify returned value between Android and iOS implementation, previously Android implementation worked differently and to the returned absolute path a 'file:' is prepended.
    This different behaviour may causes issues/bugs when handling the response from an
    unified code base point of view and also the returned uri on the Android native code
    is not generated properly using native functions to do so.
    
    This patch fixes this incoherence, but also extends the returned value to add:
    absolute path, file name, file size and the uri.
    Refactored Android code to make it easier to read and more robust to
    case sensitive file names.

--- android/src/main/java/fr/bamlab/rnimageresizer/ImageResizerModule.java
+++ android/src/main/java/fr/bamlab/rnimageresizer/ImageResizerModule.java
@@ -2,12 +2,16 @@ package fr.bamlab.rnimageresizer;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.net.Uri;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -45,19 +49,22 @@ class ImageResizerModule extends ReactContextBaseJavaModule {
                                            String compressFormatString, int quality, int rotation, String outputPath,
                                            final Callback successCb, final Callback failureCb) throws IOException {
         Bitmap.CompressFormat compressFormat = Bitmap.CompressFormat.valueOf(compressFormatString);
-        if (imagePath.startsWith(ImageResizer.FILE_PREFIX)) {
-            imagePath = imagePath.replaceFirst(ImageResizer.FILE_PREFIX, "");
-        }
+        Uri imageUri = Uri.parse(imagePath);
 
-        String resizedImagePath = ImageResizer.createResizedImage(this.context, imagePath, newWidth,
+        File resizedImage = ImageResizer.createResizedImage(this.context, imageUri, newWidth,
                 newHeight, compressFormat, quality, rotation, outputPath);
 
         // If resizedImagePath is empty and this wasn't caught earlier, throw.
-        if (resizedImagePath == null || resizedImagePath.isEmpty()) {
-            throw new IOException("Error getting resized image path");
+        if (resizedImage.isFile()) {
+            WritableMap response = Arguments.createMap();
+            response.putString("path", resizedImage.getAbsolutePath());
+            response.putString("uri", Uri.fromFile(resizedImage).toString());
+            response.putString("name", resizedImage.getName());
+            response.putDouble("size", resizedImage.length());
+            // Invoke success
+            successCb.invoke(response);
+        } else {
+            failureCb.invoke("Error getting resized image path");
         }
-
-        // Invoke success, prepending file prefix.
-        successCb.invoke(ImageResizer.FILE_PREFIX + resizedImagePath);
     }
 }
