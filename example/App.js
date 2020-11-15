7b10327331006dffc9bcb98b966306df9c3b64b7
Peter Xu <px.peter.xu@gmail.com>
2020-11-15 06:02:19 -0800

    feat: add different resize modes and onlyScaleDown options (#233)
    * feat: add different resize modes and onlyScaleDown options
    
    * Fix README and add better typings
    
    * Clean up docs
    
    Co-authored-by: Peter Xu <peter@travelchime.com>
    Co-authored-by: PierreCapo <pierrec@bam.tech>

--- example/App.js
+++ example/App.js
@@ -4,25 +4,29 @@
  */
 
 import React, {Component} from 'react';
+import {PermissionsAndroid, Platform} from 'react-native';
 import {
   StyleSheet,
   Text,
   View,
+  ScrollView,
   Image,
   Alert,
   TouchableOpacity,
   PermissionsAndroid,
   Platform
 } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 import CameraRoll from '@react-native-community/cameraroll';
 import ImageResizer from 'react-native-image-resizer';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  scrollView: {
     backgroundColor: '#F5FCFF',
+  },
+  container: {
+    paddingVertical: 100,
+    alignItems: 'center',
   },
   welcome: {
     fontSize: 20,
@@ -43,15 +47,45 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 5,
   },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  pickerView: {
+    backgroundColor: 'white',
+    padding: 5,
+    width: '50%',
+    borderColor: 'gray',
+    borderWidth: 1 ,
+  },
 });
+
+const modeOptions = ['contain', 'cover', 'stretch'].map(
+  mode => ({ label: mode, value: mode })
+);
+
+const onlyScaleDownOptions = [false, true].map(
+  onlyScaleDown => ({ label: onlyScaleDown.toString(), value: onlyScaleDown })
+);
+
+const targetSizeOptions = [
+  { label: '80x80', value: 80 },
+  { label: '5000x5000', value: 5000 },
+];
 
 export default class ResizerExample extends Component {
   constructor() {
     super();
 
     this.state = {
-      resizedImageUri: '',
-      loading: true,
+      mode: 'contain',
+      onlyScaleDown: false,
+
+      image: null,
+
+      resizeTargetSize: 80,
+      resizedImage: null,
     };
   }
 
@@ -96,12 +130,14 @@ hasAndroidPermission = async ()=> {
   return status === 'granted';
 }
 
-  resize() {
-    ImageResizer.createResizedImage(this.state.image.uri, 80, 60, 'JPEG', 100)
-      .then(({uri}) => {
-        this.setState({
-          resizedImageUri: uri,
-        });
+  resize = () => {
+    const { mode, onlyScaleDown, resizeTargetSize } = this.state;
+
+    this.setState({ resizedImage: null });
+
+    ImageResizer.createResizedImage(this.state.image.uri, resizeTargetSize, resizeTargetSize, 'JPEG', 100, 0, undefined, false, { mode, onlyScaleDown })
+      .then(resizedImage => {
+        this.setState({ resizedImage });
       })
       .catch(err => {
         console.log(err);
@@ -114,24 +150,66 @@ hasAndroidPermission = async ()=> {
   
 
   render() {
+    const { resizedImage } = this.state;
+
     return (
-      <View style={styles.container}>
+      <ScrollView style={styles.scrollView} contentContainerStyle={styles.container}>
         <Text style={styles.welcome}>Image Resizer example</Text>
         <Text style={styles.instructions}>This is the original image:</Text>
         {this.state.image ? (
-          <Image style={styles.image} source={{uri: this.state.image.uri}} />
+          <Image style={styles.image} source={{uri: this.state.image.uri}} resizeMode="contain" />
         ) : null}
         <Text style={styles.instructions}>Resized image:</Text>
-        <TouchableOpacity onPress={() => this.resize()}>
+
+        <View style={styles.row}>
+          <Text>Mode: </Text>
+
+          <RNPickerSelect
+            value={this.state.mode}
+            onValueChange={(mode) => this.setState({ mode })}
+            items={modeOptions}
+            style={{ viewContainer: styles.pickerView }}
+          />
+        </View>
+
+        <View style={styles.row}>
+          <Text>Only scale down? </Text>
+
+          <RNPickerSelect
+            value={this.state.onlyScaleDown}
+            onValueChange={(onlyScaleDown) => this.setState({ onlyScaleDown })}
+            items={onlyScaleDownOptions}
+            style={{ viewContainer: styles.pickerView }}
+          />
+        </View>
+
+        <View style={styles.row}>
+          <Text>Target size: </Text>
+
+          <RNPickerSelect
+            value={this.state.resizeTargetSize}
+            onValueChange={(resizeTargetSize) => this.setState({ resizeTargetSize })}
+            items={targetSizeOptions}
+            style={{ viewContainer: styles.pickerView }}
+          />
+        </View>
+
+        <TouchableOpacity onPress={this.resize}>
           <Text style={styles.resizeButton}>Click me to resize the image</Text>
         </TouchableOpacity>
-        {this.state.resizedImageUri ? (
-          <Image
-            style={styles.image}
-            source={{uri: this.state.resizedImageUri}}
-          />
+
+        {resizedImage ? (
+          <>
+            <Image
+              style={styles.image}
+              source={{uri: resizedImage.uri}}
+              resizeMode="contain"
+            />
+            <Text>Width: {resizedImage.width}</Text>
+            <Text>Height: {resizedImage.height}</Text>
+          </>
         ) : null}
-      </View>
+      </ScrollView>
     );
   }
 }
---
+++ example/README.md
@@ -0,0 +1,11 @@
+# Test app
+
+This is an app used to test the features of `react-native-image-resizer`.
+
+## Getting started
+
+1.  Run `yarn install`
+2.  To run on Android, run `yarn android`
+3.  To run on iOS:
+    1. Run `cd ios && pod install && cd ..` to install Cocoapods depedencies.
+    2. Run `yarn ios`
