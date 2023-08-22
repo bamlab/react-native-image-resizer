bb04979275ab7fa71a49ad93c897c9aa1229ed8f
Clément Taboulot <clement-taboulot@hotmail.fr>
2023-08-22 10:37:57 +0200

    Issue 328 ios fix image loading using uri or path (#381)
    * feat(example): load image using react-native-vision-camera
    
    * fix(ios): fix image loading
    
    Use RCTConvert & RCTUmageLoader in order to load images correctly with path and uri
    
    * feat(example): load image from url
    
    * feat(example): load image using react-native-camera-roll
    
    * fix(example): display image correctly on android
    
    * docs: specify with which library uri parameter has been tested

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -8,10 +8,12 @@
  * @format
  */
 
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import {
   Alert,
   Image,
+  PermissionsAndroid,
+  Platform,
   ScrollView,
   StyleSheet,
   Text,
@@ -24,7 +26,9 @@ import type {
   ResizeMode,
   Response,
 } from '@bam.tech/react-native-image-resizer';
-import { Asset, launchImageLibrary } from 'react-native-image-picker';
+import { launchImageLibrary } from 'react-native-image-picker';
+import { Camera, useCameraDevices } from 'react-native-vision-camera';
+import { CameraRoll } from '@react-native-camera-roll/camera-roll';
 
 const modeOptions: { label: string; value: ResizeMode }[] = [
   {
@@ -52,21 +56,66 @@ const onlyScaleDownOptions: { label: string; value: boolean }[] = [
   },
 ];
 
+async function hasCameraRollPermissions() {
+  const getCheckPermissionPromise = () => {
+    if (Platform.OS === 'ios') {
+      return true;
+    }
+
+    if (Number(Platform.Version) >= 33) {
+      return Promise.all([
+        PermissionsAndroid.check('android.permission.READ_MEDIA_IMAGES'),
+      ]).then(([hasReadMediaImagesPermission]) => hasReadMediaImagesPermission);
+    } else {
+      return PermissionsAndroid.check(
+        'android.permission.READ_EXTERNAL_STORAGE'
+      );
+    }
+  };
+
+  const hasPermission = await getCheckPermissionPromise();
+  if (hasPermission) {
+    return true;
+  }
+  const getRequestPermissionPromise = () => {
+    if (Number(Platform.Version) >= 33) {
+      return PermissionsAndroid.requestMultiple([
+        'android.permission.READ_MEDIA_IMAGES',
+      ]).then(
+        (statuses) =>
+          statuses['android.permission.READ_MEDIA_IMAGES'] ===
+          PermissionsAndroid.RESULTS.GRANTED
+      );
+    } else {
+      return PermissionsAndroid.request(
+        'android.permission.READ_EXTERNAL_STORAGE'
+      ).then((status) => status === PermissionsAndroid.RESULTS.GRANTED);
+    }
+  };
+
+  return await getRequestPermissionPromise();
+}
+
 const App = () => {
   const [selectedMode, setMode] = useState<ResizeMode>('contain');
   const [onlyScaleDown, setOnlyScaleDown] = useState(false);
-  const [image, setImage] = useState<null | Asset>();
+  const [imageUri, setImageUri] = useState<null | string>();
   const [sizeTarget, setSizeTarget] = useState(80);
   const [resizedImage, setResizedImage] = useState<null | Response>();
+  const [isCameraActive, setIsCameraActive] = useState(false);
+  const [inputImageUrl, setInputImageUrl] = useState('');
+  const devices = useCameraDevices();
+  const device = devices.back;
+  const camera = useRef<Camera>(null);
 
   const resize = async () => {
-    if (!image || !image.uri) return;
+    if (!imageUri) return;
 
     setResizedImage(null);
 
     try {
       let result = await ImageResizer.createResizedImage(
-        image.uri,
+        imageUri,
         sizeTarget,
         sizeTarget,
         'JPEG',
@@ -86,15 +135,90 @@ const App = () => {
     }
   };
 
-  const selectImage = async () => {
+  const selectImageFromPicker = async () => {
     launchImageLibrary({ mediaType: 'photo' }, (response) => {
       if (!response || !response.assets) return;
       const asset = response.assets[0];
       if (asset) {
-        setImage(asset);
+        setImageUri(asset.uri);
       }
     });
   };
+
+  const selectFirstImageFromCameraRoll = async () => {
+    const hasPermission = await hasCameraRollPermissions();
+    if (!hasPermission) {
+      Alert.prompt('No permission to access photo library');
+      return;
+    }
+
+    const result = await CameraRoll.getPhotos({ first: 1 });
+    if (result.edges.length === 0) {
+      Alert.prompt('Can not load first image from camera roll');
+    }
+
+    setImageUri(result.edges[0]?.node.image.uri);
+  };
+
+  const hasCameraPermission = async () => {
+    const cameraPermission = await Camera.getCameraPermissionStatus();
+
+    switch (cameraPermission) {
+      case 'authorized':
+        return true;
+      case 'not-determined':
+      case 'denied':
+        const newCameraPermission = await Camera.requestCameraPermission();
+        return newCameraPermission === 'authorized';
+      default:
+        Alert.alert('Go in app settings to allow camera usage');
+        return false;
+    }
+  };
+
+  const openCamera = async () => {
+    setIsCameraActive(await hasCameraPermission());
+  };
+
+  const takePhoto = async () => {
+    if (camera.current === null) {
+      return;
+    }
+
+    const photo = await camera.current.takePhoto();
+    // Adding file:// is mandatory in Android in order to display the image properly with <Image>
+    setImageUri((Platform.OS === 'android' ? 'file://' : '') + photo.path);
+    closeCamera();
+  };
+
+  const loadImageFromUrl = () => setImageUri(inputImageUrl);
+
+  const closeCamera = () => setIsCameraActive(false);
+
+  if (isCameraActive && device !== undefined) {
+    return (
+      <View style={styles.cameraContainer}>
+        <Camera
+          ref={camera}
+          style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={isCameraActive}
+          photo={true}
+          preset="medium"
+        />
+        <View style={styles.cameraButtonContainer}>
+          <TouchableOpacity style={styles.button} onPress={takePhoto}>
+            <Text>Capture</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.cameraButtonContainer}>
+          <TouchableOpacity style={styles.button} onPress={closeCamera}>
+            <Text>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 
   return (
     <ScrollView
@@ -102,14 +226,44 @@ const App = () => {
       contentContainerStyle={styles.container}
     >
       <Text style={styles.welcome}>Image Resizer example</Text>
-      <TouchableOpacity style={styles.button} onPress={selectImage}>
-        <Text>Select an image</Text>
-      </TouchableOpacity>
+      <View style={styles.imageSourceButtonContainer}>
+        <TouchableOpacity style={styles.button} onPress={selectImageFromPicker}>
+          <Text>{'Select an image (react-native-image-picker)'}</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.imageSourceButtonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={selectFirstImageFromCameraRoll}
+        >
+          <Text>
+            {
+              'Load first image of camera roll (@react-native-camera-roll/camera-roll)'
+            }
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.imageSourceButtonContainer}>
+        <TouchableOpacity style={styles.button} onPress={openCamera}>
+          <Text>Take a photo</Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={(text) => setInputImageUrl(text)}
+        placeholder="url"
+        placeholderTextColor={'grey'}
+      />
+      <View style={styles.imageSourceButtonContainer}>
+        <TouchableOpacity style={styles.button} onPress={loadImageFromUrl}>
+          <Text>Load image from url</Text>
+        </TouchableOpacity>
+      </View>
       <Text style={styles.instructions}>This is the original image:</Text>
-      {image ? (
+      {imageUri ? (
         <Image
           style={styles.image}
-          source={{ uri: image.uri }}
+          source={{ uri: imageUri }}
           resizeMode="contain"
         />
       ) : null}
@@ -147,6 +301,7 @@ const App = () => {
 
       <Text>Target size: </Text>
       <TextInput
+        style={styles.textInput}
         placeholder={sizeTarget.toString()}
         keyboardType="decimal-pad"
         onChangeText={(text) => {
@@ -178,7 +333,14 @@ const styles = StyleSheet.create({
   },
   container: {
     paddingVertical: 100,
-    alignItems: 'center',
+    paddingHorizontal: 10,
+  },
+  cameraContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  imageSourceButtonContainer: {
+    marginBottom: 10,
   },
   welcome: {
     fontSize: 20,
@@ -191,9 +353,6 @@ const styles = StyleSheet.create({
   },
   image: {
     height: 250,
-    borderWidth: 1,
-    borderColor: 'black',
-    alignSelf: 'stretch',
     marginBottom: 10,
   },
   resizeButton: {
@@ -206,6 +365,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
     paddingVertical: 20,
     borderRadius: 10,
+    alignItems: 'center',
   },
   optionContainer: {
     alignSelf: 'stretch',
@@ -213,11 +373,24 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     marginBottom: 10,
   },
+  cameraButtonContainer: {
+    marginBottom: 40,
+    marginHorizontal: 20,
+  },
   buttonOption: {
     backgroundColor: '#2596be',
     paddingHorizontal: 10,
     paddingVertical: 10,
     borderRadius: 10,
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 2,
+    margin: 10,
+    alignSelf: 'stretch',
+    textAlign: 'center',
+    overflow: 'hidden',
   },
 });
 
